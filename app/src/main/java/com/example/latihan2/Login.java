d04abcd3a75a0cfb3efaf9ed6761d1e3bde27c46
faruq <dafig57@gmail.com>
2025-05-07 20:10:27 +0700

    feat [AWQA] login-page

---
+++ app/src/main/java/com/example/latihan2/Login.java
@@ -0,0 +1,49 @@
+package com.example.latihan2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+
+    EditText edUsername;
+    EditText edPassword;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        edUsername = (EditText) findViewById(R.id.edUsername);
+        edPassword = (EditText) findViewById(R.id.edPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(edUsername.getText().toString().equals("admin")
+                        && edPassword.getText().toString().equals("admin123")) {
+                    Toast.makeText(Login.this, "Login Succes", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Login.this, Kalkulator.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(Login.this, "Login Gagal", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+}
